334725e97d708f2a1aed075529a086f96771db36
Luca <lucatesta227@gmail.com>
2023-03-29 22:00:35 -0300

    Realizando Header

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoComponent } from './componentes/logo/logo.component';
+import { RedesComponent } from './componentes/redes/redes.component';
+import { BannerComponent } from './componentes/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
